ad47cbfd130e369b6900e66c3432118e7fb46f2c
Niko Kovacevic <niko@whale.enterprises>
2017-04-07 21:05:48 +0000

    Default email set

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bob@sportsbusiness.solutions'),
+        'name' => env('MAIL_FROM_NAME', 'Sports Business Solutions'),
     ],
 
     /*
