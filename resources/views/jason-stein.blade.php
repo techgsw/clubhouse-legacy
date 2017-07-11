2219d7f0ec1b15c303de8e0abb77ba3151a0d818
Niko Kovacevic <nikovacevic@gmail.com>
2017-07-11 16:22:20 -0600

    Completed all /about and bio pages

---
+++ resources/views/jason-stein.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.default')
+@section('title', 'Jason Stein')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col s12 center-align" style="margin-top: 20px;">
+            <img src="/images/about/jason.jpg" alt="" style="width: 80%; max-width: 200px; border-radius: 50%;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <h3 style="margin: 0;">Jason Stein</h3>
+            <h5>Director of Recruiting</h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <p>Jason Stein was born and raised in Olney, MD. Growing up he played all organized sports and was a huge fan of them all too. After high school he attend the nearby University of Maryland. However, while in college, his focus shifted away from sports and towards business as he worked towards obtaining his degree. With his eyes initially set on becoming a sports agent, Jason pursued a degree in Criminology and Criminal Justice and graduated from Maryland in 2011. Throughout his time at UMD, Jason worked at the University’s Golf Course and Clubhouse until he received an internship his senior year as an executive recruiter for Federal Consulting Resources (FCR) in Washington, DC. FCR is a customer-focused executive, management and technical search firm that provides staffing and consulting services to the Federal Integration sector.</p>
+            <p>After graduating and successfully completing his internship he was offered the chance to continue his position at the firm full time, which he accepted. Over the course of five years he quickly developed as a recruiter at FCR, earning National Pacesetter Awards and ranking in the Top 20% of all Search Consultants within the MRI franchising network his first two full years. As the company moved out of the MRI Network, he developed into the Account Executive role where he was responsible for developing more than 20 new clients and generating over one million dollars in personal billings. At FCR he played a vital role in establishing relationships with current and prospective recruitment clients while also identifying, qualifying and submitting job candidates for various positions across the FCR client portfolio.</p>
+            <p>On the side Jason also aids and supports an emerging organization, Front Office Sports. Led by a group of students at the University of Miami, they are a leading educational resource for those who aspire to work in sports business. Jason has applied his recruitment and business experience towards developing strategic partnerships and relationships for FOS as the website continues to rapidly expand its presence within the sports business community.</p>
+            <p>Through a mutual partnership with Front Office Sports, Jason met Bob Hamer, the President and Founder of Sports Business Solutions. They quickly hit it off as they both are incredibly driven, they share a passion for sports business and have a desire to help others succeed. After visits to Phoenix and numerous conversations, Bob offered Jason the chance to join Sports Business Solutions full time as the Director of Recruiting as the company looked to develop a more traditional executive search and recruitment practice.</p>
+            <p>In 2016 Jason left FCR to join Sports Business Solutions full time. In his current role at SBS he’s responsible for leading all recruiting efforts. His duties include new business development, maintaining current recruiting partnerships, and sourcing new quality job candidates for Sports Business Solutions’ clients. Jason has a passion for the recruiting business. He truly enjoys helping people grow their careers, while also assisting his clients in finding the right job candidates. Having the ability to combine that with his entrepreneurial spirit and love of sports makes his new role with Sports Business Solutions a perfect fit. Jason currently resides in Gaithersburg, MD and he’s eager to help Bob and SBS build the most successful recruiting practice in sports.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <a href="/files/jason-stein-resume.pdf" target="_blank" class="btn sbs-red">Download resume</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                    <div class="collapsible-header">Interests<span style="float: right;">+</span></div>
+                    <div class="collapsible-body" style="padding-top: 0;">
+                        <p><b>Full name:</b> Jason Philip Stein</p>
+                        <p><b>Nickname(s):</b>  Jay, Jay-Bird </p>
+                        <p><b>Favorite activities:</b> Gym, traveling, playing and watching sports, watching movies, cooking, enjoying time with friends and family</p>
+                        <p><b>Favorite food:</b> Red meat</p>
+                        <p><b>Favorite color:</b> Green</p>
+                        <p><b>Favorite candy:</b> Reese’s peanut butter cups (plus any other Reese’s candy), Snickers</p>
+                        <p><b>Favorite TV shows:</b> The Wire, South Park, Seinfeld, Breaking Bad</p>
+                        <p><b>Favorite travel destinations:</b> Las Vegas, Jamaica</p>
+                        <p><b>Places I'd like to visit:</b> Spain, Beijing, Amsterdam, and The Maldives</p>
+                        <p><b>Favorite sport:</b> Football, then Basketball, Baseball and Hockey</p>
+                        <p><b>Magazine Subscriptions:</b> Sports Business Journal, SLAM, Men’s Fitness</p>
+                        <p><b>Favorite Movies:</b> The Hurricane, The Departed, Friday</p>
+                        <p><b>Favorite type of Pizza:</b> Bacon, Pepperoni and Jalapeños</p>
+                        <p><b>Favorite music:</b> 90’s Hip-Hop and R&amp;B</p>
+                        <p><b>Something most people don't know about me:</b> Nearly since birth, I have lived with no sense of smell (anosmia) due to a lack of functioning olfactory bulbs.</p>
+                        <p><b>That's a little about me, I'd love to learn more about you! Click <a href="/contact">here</a> and tell us more.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/mike-rudner.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.default')
+@section('title', 'Mike Rudner')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col s12 center-align" style="margin-top: 20px;">
+            <img src="/images/about/mike.png" alt="" style="width: 80%; max-width: 200px; border-radius: 50%;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <h3 style="margin: 0;">Mike Rudner</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <p>Mike grew up in Wayne, New Jersey, about a thirty minute drive from New York City and sports have always been a big part of his life.  Even at a young age his parents remember him running outside to grab the newspaper so he could take a deep dive into the sports section. He played just about every sport growing up, but by middle school, he knew his dreams of playing professional sports were not going to come true.  So he decided to do the next best thing...become a sportscaster.  He pursued that passion by enrolling as a Broadcast Journalism major at the prestigious Newhouse School of Communications at Syracuse University.  From there, he landed internships at WFAN and SNY in New York before graduating Syracuse Magna Cum Laude in 2007.  His first paying job out of school was as an Editorial Producer at MLBAM, Major League Baseball’s digital platform.</p>
+            <p>Mike spent a year and a half at MLBAM managing websites such as SNY.tv, AVP.com, YESNetwork.com and WCSN.com (now Universal Sports).  In addition to working at MLBAM, he hosted a very popular Mets podcast called NYMetscast.  Mike’s co-host at the time was Ted Berg, who’s now a featured writer on USA Today’s website and a frequent guest on the MLB Network.</p>
+            <p>Mike parlayed his job at MLBAM into an opportunity late 2008 managing all of the websites for the state agency known as the New Jersey Sports &amp; Exposition Authority (NJSEA).  While there he was responsible for answering all customer inquiries from the websites and managing the Facebook, Twitter and YouTube pages for each property.   The most popular question asked on the website was, “Where is the best seat for this event?”  He then decided to do some digging and stumbled across a company called IOMEDIA in New York.  They had developed a 3D Virtual Venue of Yankee Stadium that allowed fans to see 360-degree views of the stadium seats virtually, from the comfort of their own homes.</p>
+            <p>While negotiating with IOMEDIA to build a Virtual Venue of the IZOD Center in New Jersey, the owners of the company asked Mike if he’d join their team and help launch their Sports &amp; Entertainment division.  The Yankees were IOMEDIA’s only Sports client, but Mike believed in the product and company. He decided to take a leap of faith and join the digital agency in 2009.</p>
+            <p>With no prior sales experience his role early on was simply to book as many meetings as possible.  Over time, he started presenting the benefits of the product, and before long, Mike was leading the entire sales process.  What started as just one local client turned into over 100 global clients throughout Mike’s seven and a half years at IOMEDIA.  He helped the company form a partnership with Ticketmaster and led the Sales &amp; Account Management team his last two-plus years at the company.</p>
+            <p>Mike was introduced to Bob Hamer through a mutual friend and the two immediately clicked.  Mike and Bob spent several hours talking about how to grow Sports Business Solutions (SBS), including a dinner during a March 2017 blizzard in New York City.  They agreed to work together shortly thereafter and Mike couldn’t have been more excited to join the growing SBS team. Now, as the new Vice President of Business Operations at SBS he is responsible for leading all new business development and creating new revenue streams to complement the company’s growing portfolio of products and services. He’s looking forward to helping Sports Business Solutions grow into a powerhouse agency that becomes the industry standard for sports business consulting.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <a href="/files/mike-rudner-resume.pdf" target="_blank" class="btn sbs-red">Download resume</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                    <div class="collapsible-header">Interests<span style="float: right;">+</span></div>
+                    <div class="collapsible-body" style="padding-top: 0;">
+                        <p><b>Full name:</b> Michael Jason Rudner</p>
+                        <p><b>Nickname(s):</b> Mike, Mikey, Ruds</p>
+                        <p><b>Favorite activities:</b> Playing and watching sports (especially my Mets), watching TV and movies, going out with friends, bar games, traveling</p>
+                        <p><b>Favorite food:</b> Italian</p>
+                        <p><b>Favorite color:</b> Orange (Mets, Knicks, Syracuse)</p>
+                        <p><b>Favorite candy:</b> Raisinets and Butterfingers</p>
+                        <p><b>Favorite TV shows:</b> Seinfeld, Curb Your Enthusiasm, The Leftovers, Veep, Breaking Bad, Ray Donovan, Modern Family</p>
+                        <p><b>Favorite travel destinations:</b> Anywhere there’s a beach</p>
+                        <p><b>Places I'd like to visit:</b> Hawaii, France, Italy, Amsterdam, Australia….and Austin, Texas</p>
+                        <p><b>Favorite sport:</b> Football, then Basketball and Baseball</p>
+                        <p><b>Magazine Subscriptions:</b> Sports Business Journal</p>
+                        <p><b>Favorite Movies:</b> Step Brothers, 40 Year Old Virgin, Old School </p>
+                        <p><b>Favorite type of Pizza:</b> Meat Lovers</p>
+                        <p><b>Favorite music:</b> Pop</p>
+                        <p><b>Something most people don't know about me:</b> I was the co-captain of my high school bowling team and bowled competitively in college</p>
+                        <p><b>That's a little about me, I'd love to learn more about you! Click <a href="/contact">here</a> and tell us more.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
