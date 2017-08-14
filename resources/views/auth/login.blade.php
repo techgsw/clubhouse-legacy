0580bc15a6e7cfbb15a405e6051cb55805842e71
Niko Kovacevic <nikovacevic@gmail.com>
2017-08-14 16:13:03 -0600

    Added login error message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,21 @@
 @section('content')
 <div class="container">
     <h5 class="header red-text">Log In</h5>
+    @if ($errors->has('email'))
+        <div class="row">
+            <div class="col s12">
+                <div class="alert card-panel red lighten-4 red-text text-darken-4">
+                    Sorry, your email or password is incorrect.
+                </div>
+            </div>
+        </div>
+    @endif
     <div class="row">
         <div class="col s12">
             <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                 {{ csrf_field() }}
                 <div class="input-field{{ $errors->has('email') ? ' invalid' : '' }}">
-                    <label for="email">E-Mail Address</label>
+                    <label for="email">Email Address</label>
                     <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required autofocus>
                 </div>
                 <div class="input-field{{ $errors->has('password') ? ' invalid' : '' }}">
