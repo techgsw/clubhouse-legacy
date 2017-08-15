bf895d54ddc997e7e3aaac171184914e6e2604d4
Sean Brown <sean@whale.enterprises>
2017-08-15 19:32:51 +0000

    Added vendor mail views. Updated mail layout, header and footer.

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
+            <tr>
+                <td class="content-cell" align="center">
+                    <p class="small">Copyright © 2017 Sports Business Solutions | All Rights Reserved</p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,5 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">Sports Business Solutions</a>
+    </td>
+</tr>
