d37dfa3ef086c29d96478a15a92f3f81c12e4d0c
Niko Kovacevic <nikovacevic@gmail.com>
2017-08-16 16:31:51 -0600

    Updated inquirty-submitted email

--- resources/views/emails/inquiry/submitted.blade.php
+++ resources/views/emails/inquiry/submitted.blade.php
@@ -1,9 +1,9 @@
 @component('emails.layout')
     @slot('body')
         <p>{{ ucwords($user->first_name) }},</p>
-        <p>Thank you for inquiring about the {{ $job->title }} position with {{ $job->organization }}. We've received your résumé, which we're passing along to our partners for review.</p>
+        <p>Thank you for inquiring about the {{ $job->title }} position with the {{ $job->organization }}. We've received your résumé and you are currently being considered for the position.</p>
         <p><a href="{{ config('app.url') }}job/{{ $job->id }}">Click here</a> to return to the job listing and view your application.</p>
-        <p>We're committed to helping you succeed, so we're looking forward to helping you land the job you've always wanted.</p>
+        <p>We're committed to helping you succeed, so we're looking forward to helping you land your next job in sports.</p>
         <p>Regards,<br/>Sports Business Solutions</p>
     @endslot
 @endcomponent
