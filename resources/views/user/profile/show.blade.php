4e69fc582be08ae9a9ddc197d04281ffbb2fd3b5
Niko Kovacevic <nikovacevic@gmail.com>
2017-10-06 10:17:23 -0600

    Basic Employment and Educational History sections complete

--- resources/views/user/profile/show.blade.php
+++ resources/views/user/profile/show.blade.php
@@ -87,13 +87,88 @@
     <div class="row">
         <div class="col s12">
             <h4>Employment History</h4>
+            @if ($user->profile->current_organization)
+                <p>Currently works for <b>{{$user->profile->current_organization}}</b>.
+                @if ($user->profile->current_organization_years)
+                    Has been with the organization for
+                    @if ($user->profile->current_organization_years == 'less_1')
+                        <b>less than 1 year</b>.
+                    @endif
+                    @if ($user->profile->current_organization_years == '1_to_3')
+                        <b>1 to 3 years</b>.
+                    @endif
+                    @if ($user->profile->current_organization_years == '3_to_6')
+                        <b>3 to 6 years</b>.
+                    @endif
+                    @if ($user->profile->current_organization_years == '6_more')
+                        <b>more than 6 years</b>.
+                    @endif
+                @endif
+                </p>
+            @endif
+            @if ($user->profile->current_title)
+                <p>Current title is <b>{{$user->profile->current_title}}</b>.
+                @if ($user->profile->current_title_years)
+                    Has had that title for
+                    @if ($user->profile->current_title_years == 'less_1')
+                        <b>less than 1 year</b>.
+                    @endif
+                    @if ($user->profile->current_title_years == '1_to_3')
+                        <b>1 to 3 years</b>.
+                    @endif
+                    @if ($user->profile->current_title_years == '3_to_6')
+                        <b>3 to 6 years</b>.
+                    @endif
+                    @if ($user->profile->current_title_years == '6_more')
+                        <b>more than 6 years</b>.
+                    @endif
+                @endif
+                </p>
+            @endif
             <p></p>
         </div>
     </div>
     <div class="row">
         <div class="col s12">
             <h4>Educational History</h4>
-            <p></p>
+            @if ($user->profile->education_level)
+                @if ($user->profile->education_level == 'high_school')
+                    <p>Has completed <b>high school</b>.</p>
+                @endif
+                @if ($user->profile->education_level == 'associate')
+                    <p>Has completed <b>an associate's degree</b>.</p>
+                @endif
+                @if ($user->profile->education_level == 'bachelor')
+                    <p>Has completed <b>a bachelor's degree</b>.</p>
+                @endif
+                @if ($user->profile->education_level == 'master')
+                    <p>Has completed <b>a master's degree</b>.</p>
+                @endif
+                @if ($user->profile->education_level == 'doctor')
+                    <p>Has completed <b>a doctorate</b>.</p>
+                @endif
+            @endif
+            @if ($user->profile->college_name)
+                @if ($user->profile->college_graduation_year)
+                    <p>
+                    @php
+                        $today = new \DateTime('NOW');
+                        $year = (int)$today->format('Y');
+                    @endphp
+                    @if ($user->profile->college_graduation_year > $year)
+                        Plans to graduate from
+                    @else
+                        Graduated from
+                    @endif
+                        <b>{{$user->profile->college_name}}</b> in <b>{{$user->profile->college_graduation_year}}</b>
+                    </p>
+                @else
+                    <p>Attends {{$user->profile->college_name}}.</p>
+                @endif
+            @endif
+            @if ($user->profile->has_school_plans)
+                <p>Has plans to continue education.
+            @endif
         </div>
     </div>
 </div>
