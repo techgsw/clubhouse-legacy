af9e24b49ed091ab9bc5c9d7d7c75c3bf424e643
Niko Kovacevic <nikovacevic@gmail.com>
2017-10-06 09:59:34 -0600

    Profile basic info complete

--- resources/views/user/profile/show.blade.php
+++ resources/views/user/profile/show.blade.php
@@ -25,9 +25,63 @@
     </div>
     <div class="row">
         <div class="col s12">
-            <p>Lives in <b>Boulder, CO</b></p>
-            <p>Is <b>passively seeking job opportunities</b>.</p>
-            <p>Wants to work in <b>the Northwest</b>.</p>
+            @if ($user->address && ($user->address->city || $user->address->state))
+                <p>Lives in <b>{{ $user->address->city}}, {{$user->address->state}}</b>.</p>
+            @endif
+            @if ($user->profile->job_seeking_status)
+                @if ($user->profile->job_seeking_status == 'unemployed')
+                    <p>Is currently <b>unemployed, actively seeking a new job</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_status == 'employed_active')
+                    <p>Is currently <b>employed, actively seeking a new job</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_status == 'employed_passive')
+                    <p>Is currently <b>employed, passively exploring new opportunities</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_status == 'employed_future')
+                    <p>Is currently <b>employed, only open to future opportunities</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_status == 'employed_not')
+                    <p>Is currently <b>employed, currently have my dream job</b>.</p>
+                @endif
+            @endif
+            @if ($user->profile->job_seeking_region)
+                @if ($user->profile->job_seeking_region == 'mw')
+                    <p>Wants to work in the <b>Midwest</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_region == 'ne')
+                    <p>Wants to work in the <b>Northeast</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_region == 'nw')
+                    <p>Wants to work in the <b>Northwest</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_region == 'se')
+                    <p>Wants to work in the <b>Southeast</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_region == 'sw')
+                    <p>Wants to work in the <b>Southwest</b>.</p>
+                @endif
+            @endif
+            @if ($user->profile->job_seeking_type)
+                @if ($user->profile->job_seeking_type == 'internship')
+                    <p>Next career step is <b>an internship</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_type == 'entry_level')
+                    <p>Next career step is <b>an entry-level position</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_type == 'mid_level')
+                    <p>Next career step is <b>a mid-level position</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_type == 'entry_level_management')
+                    <p>Next career step is <b>an entry-level management position</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_type == 'mid_level_management')
+                    <p>Next career step is <b>a mid-level management position</b>.</p>
+                @endif
+                @if ($user->profile->job_seeking_type == 'executive')
+                    <p>Next career step is <b>an executive position</b>.</p>
+                @endif
+            @endif
         </div>
     </div>
     <div class="row">
