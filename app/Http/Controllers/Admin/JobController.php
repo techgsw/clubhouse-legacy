cf431ba7cb6e3559c884e7d621c7dbf1c9655b70
Niko Kovacevic <nikovacevic@gmail.com>
2017-11-10 15:18:17 -0700

    /admin/job search UI and back-end. Closes #75.

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -11,12 +11,23 @@ class JobController extends Controller
     /**
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $jobs = Job::orderBy('id', 'desc')->paginate(10);
+        $jobs = Job::filter($request)->orderBy('created_at', 'desc');
+        $count = $jobs->count();
+        $jobs = $jobs->paginate(15);
+
+        $searching =
+            request('job_type') && request('job_type') != 'all' ||
+            request('league') && request('league') != 'all' ||
+            request('state') && request('state') != 'all' ||
+            request('status') && request('status') != 'all' ||
+            request('organization');
 
         return view('admin/job', [
-            'jobs' => $jobs
+            'jobs' => $jobs,
+            'count' => $count,
+            'searching' => $searching
         ]);
     }
 }
