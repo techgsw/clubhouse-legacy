d5f4ee2a4d0e93402ec023cdef40d6a3a18ee799
Niko Kovacevic <nikovacevic@gmail.com>
2017-11-21 10:10:30 -0700

    Styling for view, create, and edit posts. Fixed create form view. #84

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -13,15 +13,16 @@
         <div class="col s12">
             <form method="post" action="/post" enctype="multipart/form-data">
                 {{ csrf_field() }}
-                <div class="input-field">
-                    <input id="title" type="text" class="validate {{ $errors->has('title') ? 'invalid' : '' }}" name="title" value="{{ old('title') }}" required autofocus>
-                    <label for="title" data-error="{{ $errors->first('title') }}">Title</label>
+                <div class="blog-post">
+                    <div class="input-field">
+                        <input class="title" placeholder="Title" id="title" type="text" name="title" value="{{ old('title') }}" required autofocus>
+                    </div>
+                    <div class="editor" style="outline: none;"></div>
+                    <div class="hidden">
+                        <input class="markdown" type="text" name="body" value="">
+                    </div>
                 </div>
-                <div class="editor" style="outline: none;"></div>
-                <div class="hidden">
-                    <input class="markdown" type="text" name="body" value="">
-                </div>
-                <div class="input-field">
+                <div class="input-field" style="margin-top: 40px;">
                     <button type="submit" class="btn sbs-red">Post</button>
                 </div>
             </form>
@@ -42,4 +43,28 @@
         });
     })();
 </script>
+<style type="text/css" media="screen">
+    .blog-post {
+        outline: none;
+    }
+    .blog-post .title {
+        font-size: 2.2rem;
+        letter-spacing: 0.25px;
+        border-bottom: none;
+    }
+    .blog-post .title:focus {
+        border-bottom: none !important;
+        box-shadow: none !important;
+    }
+    .blog-post h2 {
+        font-size: 1.6rem;
+        font-weight: bold;
+        color: rgba(0, 0, 0, 0.87);
+    }
+    .blog-post h3 {
+        font-size: 1.25rem;
+        font-weight: bold;
+        color: #EB2935;
+    }
+</style>
 @endsection
