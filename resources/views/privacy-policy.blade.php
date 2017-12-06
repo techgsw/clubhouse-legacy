acd43fbdabe1ce2cbe8224b68a0b699e97c97ce6
Sean Brown <sean@whale.enterprises>
2017-12-06 19:53:40 +0000

    Updated privacy policy and terms of service.

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -5,14 +5,71 @@
     <div class="row">
         <div class="col s12">
             <h3>Privacy policy</h3>
-            <ol class="spaced">
-                <li><b>Security:</b> To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered, or destroyed.  If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption.  Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.</li>
-                <li><b>Cookies</b>  Here is a list of cookies that we use.  We’ve listed them here so you that you can choose if you want to opt-out of cookies or not: <code>_secure_session_id, unique token, sessional storefront_digest, unique token, indefinite</code></li>
-                <li><b>Passwords:</b> If this website has a username and/or password, this is used to determine if the current visitor has access.</li>
-                <li><b>Age of Consent:</b>  By using this website, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this website.</li>
-                <li><b>Modifications:</b>  We reserve the right to modify this privacy policy at any time, so please review it frequently.  Changes and clarifications will take effect immediately upon their posting on this website.  If we make material changes to this Privacy Policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.  If SBS is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.</li>
-                <li><b>Contact Information:</b>  If you would like to access, correct, amend, or delete any personal information we have about you, register a complaint, or simply want more information, contact our Privacy Compliance Officer at support@sportsbusiness.solutionssolutions</li>
-            </ol>
+            <p><b>Introduction.</b> Sports Business Solutions, LLC, an Arizona limited liability company (”Company” or “We”) respect your privacy and are committed to protecting it through our compliance with this policy.</p>
+            <p>This policy describes the types of information we may collect from you or that you may provide when you visit the website https://sportsbusiness.solutions (our “Website”) and our practices for collecting, using, maintaining, protecting, and disclosing that information.</p>
+            <p>This policy applies to information we collect:</p>
+            <ul class="browser-default">
+                <li><p>On this Website.</p></li>
+                <li><p>In email, text, and other electronic messages between you and this Website.</p></li>
+            </ul>
+            <p>Please read this policy carefully to understand our policies and practices regarding your information and how we will treat it. If you do not agree with our policies and practices, your choice is not to use our Website. By accessing or using this Website, you agree to this privacy policy. This policy may change from time to time (see Changes to Our Privacy Policy). Your continued use of this Website after we make changes is deemed to be acceptance of those changes, so please check the policy periodically for updates.</p>
+            <p><b>Children Under the Age of 13.</b> Our Website is not intended for children under 13 years of age. No one under age 13 may provide any information to or on the Website. We do not knowingly collect personal information from children under 13. If you are under 13, do not use or provide any information on this Website or on or through any of its features/register on the Website, use any of the interactive or public comment features of this Website or provide any information about yourself to us, including your name, address, telephone number, email address, or any screen name or user name you may use. If we learn we have collected or received personal information from a child under 13 without verification of parental consent, we will delete that information.</p>
+            <p><b>Information We Collect About You and How We Collect It.</b> We collect several types of information from and about users of our Website, including information:</p>
+            <ul class="browser-default">
+                <li><p>by which you may be personally identified, such as name, username, and e-mail address (”personal information”);</p></li>
+                <li><p>that is about you but individually does not identify you; and/or</p></li>
+                <li><p>about your internet connection, the equipment you use to access our Website and usage details.</p></li>
+            </ul>
+            <p>We collect this information:</p>
+            <ul class="browser-default">
+                <li><p>Directly from you when you provide it to us.</p></li>
+                <li><p>Automatically as you navigate through the site. Information collected automatically may include usage details, IP addresses, and information collected through cookies, and other tracking mechanisms.</p></li>
+            </ul>
+            <p><b>Information You Provide to Us.</b> The information we collect on or through our Website may include:</p>
+            <ul class="browser-default">
+                <li><p>Information that you provide by filling in forms on our Website. This includes information provided at the time of posting material on our public forums.</p></li>
+                <li><p>Records and copies of your correspondence (including email addresses), if you contact us.</p></li>
+            </ul>
+            <p><b>Information We Collect Through Automatic Data Collection Technologies.</b> As you navigate through and interact with our Website, we may use automatic data collection technologies to collect certain information about your equipment, browsing actions, and patterns, including:</p>
+            <ul class="browser-default">
+                <li><p>Details of your visits to our Website and other communication data and the resources that you access and use on the Website.</p></li>
+                <li><p>Information about your computer and internet connection, including your IP address, operating system, and browser type.</p></li>
+            </ul>
+            <p>The information we collect helps us to improve our Website and to deliver a better and more personalized service, including by enabling us to:</p>
+            <ul class="browser-default">
+                <li><p>Estimate our audience size and usage patterns.</p></li>
+                <li><p>Store information about your preferences.</p></li>
+                <li><p>Speed up your searches.</p></li>
+                <li><p>Recognize you when you return to our Website.</p></li>
+            </ul>
+            <p>The technologies we use for this automatic data collection may include:</p>
+            <ul class="browser-default">
+                <li><p>Cookies (or browser cookies). A cookie is a small file placed on the hard drive of your computer. You may refuse to accept browser cookies by activating the appropriate setting on your browser. However, if you select this setting you may be unable to access certain parts of our Website. Unless you have adjusted your browser setting so that it will refuse cookies, our system will issue cookies when you direct your browser to our Website.</p></li>
+            </ul>
+            <p><b>How We Use Your Information.</b> We use information that we collect about you or that you provide to us, including any personal information:</p>
+            <ul class="browser-default">
+                <li><p>To present our Website and its contents to you.</p></li>
+                <li><p>To provide you with information, products, or services that you request from us.</p></li>
+                <li><p>To fulfill any other purpose for which you provide it.</p></li>
+                <li><p>To allow you to participate in interactive features on our Website.</p></li>
+                <li><p>In any other way we may describe when you provide the information.</p></li>
+                <li><p>For any other purpose with your consent.</p></li>
+            </ul>
+            <p><b>Disclosure of Your Information.</b> We may disclose personal information that we collect or you provide as described in this privacy policy:</p>
+            <ul class="browser-default">
+                <li><p>To contractors, service providers, and other third parties we use to support our business.</p></li>
+                <li><p>To a buyer or other successor in the event of a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which personal information held by us about our Website users is among the assets transferred.</p></li>
+                <li><p>To fulfill the purpose for which you provide it.</p></li>
+                <li><p>For any other purpose disclosed by us when you provide the information.</p></li>
+                <li><p>With your consent.</p></li>
+            </ul>
+            <p>We may also disclose your personal information:</p>
+            <ul class="browser-default">
+                <li><p>To comply with any court order, law, or legal process, including to respond to any government or regulatory request.</p></li>
+                <li><p>To enforce or apply our terms of use.</p></li>
+            </ul>
+            <p><b>Your California Privacy Rights.</b> California Civil Code Section § 1798.83 permits users of our Website that are California residents to request certain information regarding our disclosure of personal information to third parties for their direct marketing purposes. To make such a request, please send an email to support@sportsbusiness.solutions</p>
+            <p><b>Changes to Our Privacy Policy.</b> It is our policy to post any changes we make to our privacy policy on this page with a notice that the privacy policy has been updated on the Website home page. The date the privacy policy was last revised is identified at the top of the page.</p>
         </div>
     </div>
 </div>
