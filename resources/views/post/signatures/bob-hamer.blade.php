dd4a3e155e577692a8a2a5dc7882b10f97930207
Sean Brown <sean@whale.enterprises>
2017-12-16 00:25:22 +0000

    Changed post signature links to use target _blank

--- resources/views/post/signatures/bob-hamer.blade.php
+++ resources/views/post/signatures/bob-hamer.blade.php
@@ -4,10 +4,10 @@
             <h5><a href="/bob-hamer">Bob Hamer</a></h5>
             <p style="margin-top: 0; margin-bottom: 0;">Founder & President<br />Sports Business Solutions</p>
             <p style="margin-top: 5px;">
-                <a href="https://facebook.com/sportsbusinesssolutions"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
-                <a href="https://twitter.com/SportsBizSol"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
-                <a href="https://instagram.com/sportsbizsol"><i class="fa fa-instagram fa-16x" aria-hidden="true"></i></a>
-                <a href="https://www.linkedin.com/company/sports-business-solutions"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://facebook.com/sportsbusinesssolutions"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://twitter.com/SportsBizSol"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://instagram.com/sportsbizsol"><i class="fa fa-instagram fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/company/sports-business-solutions"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
             </p>
         </div>
     </div>
