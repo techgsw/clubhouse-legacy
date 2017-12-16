6aa89efaec859684d9294091148e1c88c2151bd4
Sean Brown <sean@whale.enterprises>
2017-12-16 00:53:09 +0000

    Fixed merge conflict error that took image uploat out of form.

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -32,20 +32,20 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col s12">
-            <div class="file-field input-field very-small">
-                <div class="btn white black-text">
-                    <span>Edit<span class="hide-on-small-only"> Image</span></span>
-                    <input type="file" name="image_url" value="{{ old('image_url') }}">
-                </div>
-                <div class="file-path-wrapper">
-                    <input class="file-path validate" type="text" name="image_url_text" value="{{ old('image_url_text') }}">
+    <form method="post" action="/post/{{$post->title_url}}" enctype="multipart/form-data">
+        <div class="row">
+            <div class="col s12">
+                <div class="file-field input-field very-small">
+                    <div class="btn white black-text">
+                        <span>Edit<span class="hide-on-small-only"> Image</span></span>
+                        <input type="file" name="image_url" value="{{ old('image_url') }}">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text" name="image_url_text" value="{{ old('image_url_text') }}">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <form method="post" action="/post/{{$post->title_url}}" enctype="multipart/form-data">
         <div class="row">
             <div class="col s12">
                 {{ csrf_field() }}
