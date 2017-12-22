4944ed701ae264eae2a81c37c5e30b17262c4851
Niko Kovacevic <nikovacevic@gmail.com>
2017-12-22 10:07:17 -0700

    #93 archives: videos

--- resources/views/archives/index.blade.php
+++ resources/views/archives/index.blade.php
@@ -250,5 +250,33 @@
             <hr />
         </div>
     </div>
+    <div class="row">
+        <div class="col s6 m4">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/ekb485jEUa0" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col s6 m4">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/j_5iRA5Mvhw" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col s6 m4">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/qetx_GkXfiU" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col s6 m4">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/PeDsajOlRw0" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="col s6 m4">
+            <div class="video-wrapper">
+                <iframe src="https://www.youtube.com/embed/tEhNxzVTE94" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+
+    </div>
 </div>
 @endsection
