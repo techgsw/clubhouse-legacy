9e9932c46327cb44c7db8c7e3f56d80a6bead5f9
Niko Kovacevic <nikovacevic@gmail.com>
2018-01-11 08:48:31 -0700

    #96 addded feature/unfeature/rank buttons to admin job board

--- resources/views/components/admin-job-list-item.blade.php
+++ resources/views/components/admin-job-list-item.blade.php
@@ -5,7 +5,7 @@
         </a>
     </div>
     <div class="col s9 m10">
-        <p class="small" style="float: right; display: inline-block;">
+        <div class="small" style="float: right;">
             @can ('close-job', $job)
                 @if (is_null($job->open) || $job->open == false)
                     <a href="/job/{{ $job->id }}/open" class="small flat-button green"><i class="fa fa-arrow-circle-up"></i> Open</a>
@@ -16,8 +16,15 @@
             @endcan
             @can ('edit-job', $job)
                 <a href="/job/{{ $job->id }}/edit" class="small flat-button blue"><i class="fa fa-pencil"></i> Edit</a>
+                @if ($job->featured)
+                    <a href="/job/{{ $job->id }}/unfeature" class="flat-button small blue"><i class="fa fa-star"></i> {{ $job->rank }}</a>
+                    <a href="/job/{{ $job->id }}/rank-up" class="flat-button small blue"><i class="fa fa-arrow-up"></i></a>
+                    <a href="/job/{{ $job->id }}/rank-down" class="flat-button small blue"><i class="fa fa-arrow-down"></i></a>
+                @else
+                    <a href="/job/{{ $job->id }}/feature" class="flat-button small blue"><i class="fa fa-star-o"></i></a>
+                @endif
             @endcan
-        </p>
+        </div>
         <a href="/job/{{$job->id}}">
             <h5>{{ $job->title }}</h5>
             <p><span class="heavy">{{ $job->organization }}</span> in {{ $job->city }}, {{ $job->state }}</p>
@@ -25,12 +32,18 @@
         <p class="small">listed on {{ $job->created_at->format('F j, Y g:ia') }}</p>
         @can ('close-job', $job)
             <p class="small heavy" style="padding-top: 6px;">
+                @if ($job->isNew())
+                    <span class="small flat-button blue inverse" style="letter-spacing: 0.6px;"><b>New</b></span>
+                @endif
+                @if ($job->featured)
+                    <span class="small flat-button red inverse" style="letter-spacing: 0.6px;"><b>Featured</b></span>
+                @endif
                 @if (is_null($job->open))
-                    <span class="spaced small flat-button black inverse">Not open</span>
+                    <span class="small flat-button black inverse">Not open</span>
                 @elseif ($job->open == false)
-                    <span class="small flat-button red spaced inverse">Closed</span>
+                    <span class="small flat-button red inverse">Closed</span>
                 @else
-                    <span class="small flat-button green spaced inverse">Open</span>
+                    <span class="small flat-button green inverse">Open</span>
                 @endif
                 <a href="/job/{{ $job->id }}#applications" class="small flat-button black inverse">{{ count($job->inquiries) == 1 ? "1 inquiry" : count($job->inquiries) . " inquiries" }}</a>
                 <a href="/job/{{ $job->id }}?rating=none&sort=recent#applications" class="small flat-button black"><i class="fa fa-circle-thin"></i> {{ $job->inquiryTotals()['none'] }}</a>
