5da7b6d3b0c808dbe4a49220ff3e0bfaec78ce5e
Niko Kovacevic <nikovacevic@gmail.com>
2018-01-11 09:07:18 -0700

    #98 removed Bob's name from registration email

--- resources/views/emails/internal/registration.blade.php
+++ resources/views/emails/internal/registration.blade.php
@@ -1,6 +1,5 @@
 @component('emails.layout')
     @slot('body')
-	<p>Bob,</p>
-	<p>{{ ucwords($user->first_name) }} {{ ucwords($user->last_name) }} (<a href="mailto:{{ $user->email }}">{{ $user->email }}</a>) just registred.</p>
+	    <p>{{ ucwords($user->first_name) }} {{ ucwords($user->last_name) }} (<a href="mailto:{{ $user->email }}">{{ $user->email }}</a>) just registred.</p>
     @endslot
 @endcomponent
