158ebfcf4c520efef414cc3a86791de44d5a6157
cam <cameron@whale.enterprises>
2018-01-31 14:59:56 -0700

    Added contact_relationship table and wired up relationship between users and contacts. Added the mapping in the app/models between user and contact objects so they can be retrieved. #101

---
+++ database/migrations/2018_01_31_141240_create_contact_relationship_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactRelationshipTable extends Migration
+{
+    /** 
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Schema::create('contact_relationship', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->integer('contact_id')->unsigned();
+            $table->foreign('contact_id')->references('id')->on('contact');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->timestamps();
+        }); 
+    }   
+
+    /** 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::dropIfExists('contact_relationship');
+    }   
+}
