b01b21924f8cb0d66ffafa40429ead7954742de5
cam <cameron@whale.enterprises>
2018-02-08 12:53:43 -0700

    Added functionality in admin controller and contact controller to post new contact_user relationships. Added methods to sbs.js to use the new endpoints. Updated places where profile-notes class was being used to use contact-notes. #101

---
+++ app/ContactRelationship.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactRelationship extends Model
+{
+    protected $table = 'contact_relationship';
+    protected $guarded = [];
+    protected $hidden = [];
+
+}
--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -4,9 +4,6 @@
 @section('content')
 <div class="container">
     @component('contact.header', ['contact' => $contact])
-        @can ('view-profile-notes')
-            <button class="view-contact-notes-btn flat-button black" contact-id="{{ $contact->id }}">{{ $contact->getNoteCount() }} <i class="fa fa-comments"></i></button>
-        @endif
         @if ($contact->user->profile->resume_url)
             <a href="{{ Storage::disk('local')->url($contact->user->profile->resume_url) }}" class="flat-button black"><span class="hide-on-small-only">View </span> Resume</a>
         @else
@@ -27,21 +24,22 @@
         @endif
     </ul>
     @if ($contact)
-        @can ('edit-user-contacts')
+        @can ('view-profile-notes')
             <div class="row" style="margin-bottom: 0;">
                 <div class="input-field col s12 m4 l3">
-                    <form id="create-tag" action="/tag" method="post">
+                    <form id="create-contact-relationship" action="/contact/refer" method="post">
+                        <input type="hidden" id="contact_id" value="{{ $contact->id }}" />
                         {{ csrf_field() }}
                         <i class="fa fa-tags fa-small prefix" style="font-size: 1.5rem; margin-top: 12px;" aria-hidden="true"></i>
-                        <input type="text" id="tag-autocomplete-input" class="tag-autocomplete">
-                        <label for="tag-autocomplete-input">Reference Users</label>
+                        <input type="text" id="admin-user-autocomplete-input" class="admin-user-autocomplete">
+                        <label for="admin-user-autocomplete-input">Reference Users</label>
                     </form>
                 </div>
                 <div class="col s12 m8 l9">
-                    <div class="post-tags" style="position: relative; margin-top: 26px;">
+                    <div class="contact-user-relationships" style="position: relative; margin-top: 26px;">
                         @foreach ($contact->getUsers as $user)
                             <span class="flat-button gray small tag">
-                                <button type="button" name="button" class="x" tag-name="{{ $user->name }}">&times;</button>{{ $user->name }}
+                                <button type="button" name="button" class="x" admin-user-id="{{ $user->id }}">&times;</button>{{ $user->getName() }}
                                 <!-- <a href="/user/{{ $user->id }}/profile" class="flat-button black" style="display: inline-block; margin: 4px;">{{ $user->getName() }}</a> --!>
                             </span>
                         @endforeach
@@ -65,7 +63,7 @@
                     </div>
                 </div>
             @else
-                <div class="contact-notes-container">
+                <div class="contact-notes-container" style="max-height:450px; padding:0px;">
                     <div class="row">
                         <div class="col s12">
                             @foreach ($notes as $note)
