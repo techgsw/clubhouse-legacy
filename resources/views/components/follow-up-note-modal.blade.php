5df78bdd01ee53f76f8f9fab8af5e8db08761336
Niko Kovacevic <niko@whale.enterprises>
2018-04-06 10:29:02 -0600

    #114 Fixed UI for follow up modal

--- resources/views/components/follow-up-note-modal.blade.php
+++ resources/views/components/follow-up-note-modal.blade.php
@@ -1,12 +1,14 @@
-<div class="follow-up-note-modal modal modal-large modal-fixed-footer" style="max-height: 250px;">
-    <div class="row">
-        <div class="input-field col s12">
-            <form id="create-follow-up-note" method="post">
-                {{ csrf_field() }}
-                <textarea id="note" style="min-height: 150px" name="note" placeholder="New note"></textarea>
-                <input type="hidden" name="contact_id" value="" />
-                <button type="button" name="save" class="btn sbs-red submit-follow-up-note-btn float-right">Done</button>
-            </form>
-        </div>
+<div class="follow-up-note-modal modal">
+    <div class="modal-content"
+        <form id="create-follow-up-note" method="post">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="input-field col s12">
+                    <input type="hidden" name="contact_id" value="" />
+                    <textarea id="note" rows="6" name="note" placeholder="How did the follow-up go?"></textarea>
+                    <button type="button" name="save" class="btn sbs-red submit-follow-up-note-btn float-right">Close follow-up</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
