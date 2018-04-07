c6968794dffc864ce73b4060ba960d5094e34637
Niko Kovacevic <niko@whale.enterprises>
2018-04-06 18:03:45 -0600

    #125 Added default signature

---
+++ resources/views/post/signature.blade.php
@@ -0,0 +1,22 @@
+@if ($post->user->id === 1 && is_null($post->authored_by))
+    @include('post.signatures.bob-hamer')
+@elseif ($post->user->id === 7 && is_null($post->authored_by))
+    @include('post.signatures.mike-rudner')
+@elseif ($post->user->id === 2321 && is_null($post->authored_by))
+    @include('post.signatures.adam-vogel')
+@else
+    <div class="container">
+        <div class="row">
+            <div class="col s6">
+                <h5><a href="/about">{{$post->authored_by ?: $post->user->getName() }}</a></h5>
+                <p style="margin-top: 0; margin-bottom: 0;">Sports Business Solutions</p>
+                <p style="margin-top: 5px;">
+                    <a target="_blank" href="https://facebook.com/sportsbusinesssolutions"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
+                    <a target="_blank" href="https://twitter.com/SportsBizSol"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
+                    <a target="_blank" href="https://instagram.com/sportsbizsol"><i class="fa fa-instagram fa-16x" aria-hidden="true"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/company/sports-business-solutions"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
+                </p>
+            </div>
+        </div>
+    </div>
+@endif
---
+++ resources/views/post/signatures/adam-vogel.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col s6">
+            <h5><a href="/adam-vogel">Adam Vogel</a></h5>
+            <p style="margin-top: 0; margin-bottom: 0;">Sr. Director, Training & Development<br />Sports Business Solutions</p>
+            <p style="margin-top: 5px;">
+                <a target="_blank" href="https://facebook.com/sportsbusinesssolutions"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://twitter.com/SportsBizSol"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://instagram.com/sportsbizsol"><i class="fa fa-instagram fa-16x" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/company/sports-business-solutions"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
+            </p>
+        </div>
+    </div>
+</div>
