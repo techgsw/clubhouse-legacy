58872fb69d17c40799808620dfa5e2e52330500b
Niko Kovacevic <niko@whale.enterprises>
2018-04-25 13:00:02 -0600

    #134 #135 Added uncommitted files

---
+++ app/Mail/Admin/RegistrationNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail\Admin;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RegistrationNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $registrant;
+
+    public function __construct(User $registrant)
+    {
+        $this->registrant = $registrant;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('app@sportsbusiness.solutions')
+            ->markdown('emails.internal.registration');
+    }
+}
