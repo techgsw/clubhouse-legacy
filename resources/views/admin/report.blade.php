309c05961c23fc68c0158aad0e3153bea0f9b9bf
Niko Kovacevic <niko@whale.enterprises>
2018-04-30 16:28:26 -0600

    #130 CSS tweaks and cache busting

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -6,8 +6,7 @@
     <div class="row">
         <div class="col s12 m4">
             <div class="form-group">
-                <label for="date-range">Date range</label>
-                <input class="drp" type="text" name="date_range" id="date-range" />
+                <input class="drp center-align" type="text" name="date_range" id="date-range" />
                 <input class="hidden" type="text" name="date_range_start" id="date-range-start" value="{{ $start_date->format('Y-m-d') }}"/>
                 <input class="hidden" type="text" name="date_range_end" id="date-range-end" value="{{ $end_date->format('Y-m-d') }}"/>
             </div>
