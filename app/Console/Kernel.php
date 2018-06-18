08ca86fbc6d53a3a9d1339c66239dfaa7ef9724c
Niko Kovacevic <niko@whale.enterprises>
2018-06-18 15:11:38 -0600

    Added ImageServiceProvider to ConsoleKernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Providers\EmailServiceProvider;
+use App\Providers\ImageServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Log;
