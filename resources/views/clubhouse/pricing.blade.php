a5e3873ebea1cf9e565336d5d1673bf777a6920c
Sean Brown <sean@whale.enterprises>
2018-09-07 09:04:51 -0700

    Added pricing content.

--- resources/views/clubhouse/pricing.blade.php
+++ resources/views/clubhouse/pricing.blade.php
@@ -3,8 +3,8 @@
 @section('hero')
     <div class="row hero bg-image services">
         <div class="col s12">
-            <h4 class="header">Clubhouse</h4>
-            <p>Welcome to the Clubhouse. Take a look below to checkout our product offerings.</p>
+            <h4 class="header">Pricing</h4>
+            <p>The Clubhouse is the new home for aspiring and current sports business professionals to network, learn and grow their career in sports.</p>
         </div>
     </div>
 @endsection
@@ -16,38 +16,41 @@
             </div>
         </div>
         <div class="row">
-            <div class="col s12 m4">
+            <div class="col s12 m6">
                 <div class="card">
                     <div class="card-content center-align" style="display: flex; flex-flow: column; justify-content: space-between;">
                         <div class="col s12 center-align" style="padding: 10px 0 50px 0;">
-                            <p><strong>Offering one</strong></p>
+                            <h5><strong>The Clubhouse Community - Free</strong></h5>
+                            <p>Get into the game and be a part of our sports business community.</p>
+                            <ul class="">
+                                <li>Apply to open jobs</li>
+                                <li>Access our blog content and industry resources</li>
+                                <li>Purchase career services</li>
+                                <li>Receive our periodic e-Newsletter</li>
+                            </ul>
                         </div>
                         <div class="col s12 center-align" style="padding-bottom: 10px;">
-                            <a href="/job" class="btn sbs-red" style="margin-top: 20px;"> Get started</a>
+                            <a href="/" class="btn sbs-red" style="margin-top: 20px;"> Get started</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m6">
                 <div class="card">
                     <div class="card-content center-align" style="display: flex; flex-flow: column; justify-content: space-between;">
                         <div class="col s12 center-align" style="padding: 10px 0 50px 0;">
-                            <p><strong>Offering two</strong></p>
+                            <h5><strong>The Clubhouse Pro - $7/month</strong></h5>
+                            <p>Ideal for those looking for hands on opportunities and networking access to launch or transform their career in sports.</p>
+                            <ul class="">
+                                <li>Exclusive 1:1 mentorship with industry professionals</li>
+                                <li>Exclusive access to webinars and events,, featuring hot industry topics and hosted by some of the best and brightest minds in sports biz</li>
+                                <li>50% off all 1:1 career services (save $15 to $150!)</li>
+                                ne free 30 minute career Q&A with a member of our executive team
+                                <li>Access to our monthly webinar for career coaching, hosted by the Sports Business Solutions team</li>
+                            </ul>
                         </div>
                         <div class="col s12 center-align" style="padding-bottom: 10px;">
-                            <a href="/job" class="btn sbs-red" style="margin-top: 20px;"> Checkout now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content center-align" style="display: flex; flex-flow: column; justify-content: space-between;">
-                        <div class="col s12 center-align" style="padding: 10px 0 50px 0;">
-                            <p><strong>Offering three</strong></p>
-                        </div>
-                        <div class="col s12 center-align" style="padding-bottom: 10px;">
-                            <a href="/job" class="btn sbs-red" style="margin-top: 20px;"> Checkout now</a>
+                            <a href="#" class="btn sbs-red" style="margin-top: 20px;"> Checkout now</a>
                         </div>
                     </div>
                 </div>
