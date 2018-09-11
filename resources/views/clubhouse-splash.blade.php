d52a3ee3f4bf008d73694c8186368f5039f87b1f
Sean Brown <sean@whale.enterprises>
2018-09-11 16:27:04 -0700

    Started work on the clubhoue splash page.

---
+++ resources/views/clubhouse-splash.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('content')
+<div class="container-fluid" style="min-height: 500px; margin-top: -1rem; background-color: rgba(115,30,30,0.35); background-size: cover; background-image: url('https://images.unsplash.com/photo-1420177743490-15ee9ba8c78f?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&s=e77f65afae77c8831feef45288a80498&h=1500&w=2000&fit=clip&fm=jpg'); background-position: 50% 50%; background-repeat: no-repeat no-repeat">
+    <div class="row" style="margin-bottom: 0;">
+        <div class="col s12">
+            <h1>hi</h1>
+        </div>
+    </div>
+</div>
+@endsection
