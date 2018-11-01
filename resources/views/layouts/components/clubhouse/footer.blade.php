591daf38ceb5638f2f555adb39ecccdaf1bf4ab6
Sean Brown <sean@whale.enterprises>
2018-11-01 16:57:26 -0700

    Updated the footer. See #200

--- resources/views/layouts/components/clubhouse/footer.blade.php
+++ resources/views/layouts/components/clubhouse/footer.blade.php
@@ -10,18 +10,15 @@
         <div class="row">
             <div class="col s12">
                 <ul class="footer-nav">
-                    <li><a href="/refund-policy-2">Refund policy</a></li>
-                    <li><a href="/privacy-policy">Privacy policy</a></li>
                     <li><a href="/terms-of-service">Terms and conditions</a></li>
-                    <li><a href="tel:6023501223">(602)-350-1223</a></li>
-                    <li><a href="mailto:support@sportsbusiness.solutions">support@sportsbusiness.solutions</a></li>
+                    <li><a href="mailto:clubhouse@sportsbusiness.solutions">clubhouse@sportsbusiness.solutions</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
-            <p class="small">Copyright &copy; 2017 Sports Business Solutions | All Rights Reserved</p>
+            <p class="small">Copyright &copy; 2018 Sports Business Solutions | All Rights Reserved</p>
         </div>
     </div>
 </footer>
