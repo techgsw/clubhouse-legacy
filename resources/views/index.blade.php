d45ae8c0b2544ff1d2c20ac90cd80d32a2e64736
Sean Brown <sean@whale.enterprises>
2019-01-04 00:48:36 -0700

    Updated clients image on home page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,7 +40,7 @@
     </div>
     <div class="row">
         <div class="col s12 center-align">
-            <img src="/images/home/clients.png?v=5" alt="" style="margin-bottom: 60px;">
+            <img src="/images/home/clients.png?v=6" alt="" style="margin-bottom: 60px;">
         </div>
     </div>
 </div>
