e35aef17b94ddc3a726ddb87f1d7cab4005a0135
Austin <austin@whale.enterprises>
2019-06-10 17:36:18 -0700

    Removed Nick Hayden. Updated Josh Belkoff. Added Andrea St. Ubrain to about. #305

---
+++ resources/views/andrea-urbain.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.default')
+@section('title', 'Andrea St. Urbain')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col s12 center-align" style="margin-top: 20px;">
+            <img src="/images/about/andrea-urbain.jpg" alt="" style="width: 80%; max-width: 200px; border-radius: 50%;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <h3 style="margin: 0;">Andrea St. Urbain</h3>
+            <h5>Consultant, Premium Service &amp; Retention</h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <p>Andrea St. Urbain (Smith) is the newest member of the Sports Business Solutions team, and she brings with her more than 15 years’ experience in sports to the group. In 2019, prior to partnering with SBS, she started a consulting business of her own called Premium Sports Business Consulting. In that role she specialized in providing premium sales and service strategies and solutions for sports organizations and entertainment venues worldwide. </p>
+            <p>After successfully building her consulting business she got connected to Bob Hamer through a mutual contact. After a couple of conversations with Bob, they quickly realized they had similar goals and aspirations and decided it’d be great for both sides to join forces, in an effort to make an even bigger impact across the industry.</p>
+            <p>Before getting into consulting, she served as the Senior Director of Premium Services for AEG at STAPLES Center & Microsoft Theater in Los Angeles, CA from 2013-2018.</p>
+            <p>Andrea began her career as an intern with the NFL’s San Diego Chargers in 2003 and the AVP Pro Beach Volleyball Tour in 2004.  By 2005, she joined the WNBA’s Los Angeles Sparks as the Community Relations Coordinator and after two seasons transitioned to Manager of Client Relations.  In 2007, she started at STAPLES Center as an Account Manager for Premium Services.  In 2010, she moved on to Petco Park to oversee suites and key corporate partnerships for MLB’s San Diego Padres. Four seasons later she returned to STAPLES Center to manage her previous department.</p>
+            <p>Originally from Buffalo, NY, Andrea earned a Bachelor of Science degree in Sport Management from Cortland State University (SUNY) and captained the Women’s Volleyball Team. She was inducted into Frontier High School’s Wall of Fame in 2015 for her achievements as a three-sport athlete, and she currently serves as a Mentor in the WISE (Women in Sports and Events) Mentorship Program.</p>
+            <p>She grew up, and still is, immersed in the world of sports and comes from a family of talent evaluators. Her father A.J. Smith was an Executive in the NFL for 30 years and her brother Kyle Smith is the Director of College Scouting for the Washington Redskins. Andrea and her husband Noah, a Scout for the Chicago White Sox, reside in Southern California with their daughter Reese. </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 center-align">
+            <!-- Need resume -->
+            <a href="#" target="_blank" class="btn sbs-red">Download resume</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                    <div class="collapsible-header">Interests<span style="float: right;">+</span></div>
+                    <div class="collapsible-body" style="padding-top: 0;">
+                        <p><b>Full name:</b> Andrea St. Urbain</p>
+                        <p><b>Nickname(s):</b> Dre</p>
+                        <p><b>Favorite activities:</b> Spending time with my family and friends, going to sporting events, traveling, pool/beach days, volunteering with WISE (Women in Sports and Events).</p>
+                        <p><b>Favorite food:</b> Pizza</p>
+                        <p><b>Favorite color:</b> Blue</p>
+                        <p><b>Favorite candy:</b> Reese's</p>
+                        <p><b>Favorite TV shows:</b> My husband and I love to binge watch shows... Our favorites has been Breaking Bad, MadMen, Dexter, and House of Cards... Nowadays we watch a lot of the Disney channel with  our toddler!</p>
+                        <p><b>Favorite travel destinations:</b> Maui</p>
+                        <p><b>Places I'd like to visit:</b>  More of Europe</p>
+                        <p><b>Favorite sport:</b> To play - volleyball. To watch - football</p>
+                        <p><b>Magazine Subscriptions:</b> SUCCESS</p>
+                        <p><b>Favorite Movies:</b> Anything from the 90's... My brother and I have the gift of reciting every line to Dumb & Dumber, Ace Venture, or What About Bob. Our parents are very proud... </p>
+                        <p><b>Favorite type of Pizza:</b> I'm from Buffalo... We live for pizza and wings! Favorite type is classic cheese and pepperoini.</p>
+                        <p><b>Favorite music:</b> I love all genres of music. Depends on occassion or mood. My favorite bands are The Killers and Dave Matthews Band.</p>
+                        <p><b>Something most people don't know about me:</b> I started college as an Elementary Education major before switching to Sport Management my sophomore year.</p>
+                        <p><b>That's a little about me, I'd love to learn more about you! Click <a href="/contact">here</a> and tell us more.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
