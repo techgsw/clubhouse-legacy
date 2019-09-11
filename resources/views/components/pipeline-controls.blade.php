3257d7cf7d775dcf99e4f661b5d614333c157766
James Ford <jford@albany.edu>
2019-09-11 12:56:27 -0400

    Fixed bug where comms warning was showing up when clicking no-comms thumbs up on job inquiry review

--- resources/views/components/pipeline-controls.blade.php
+++ resources/views/components/pipeline-controls.blade.php
@@ -13,7 +13,7 @@
     @endif
     <!-- end halt (thumbs down) comms -->
     <!-- halt (thumbs down) no comms -->
-    <button data-action="" data-comm-type="none" data-type="{{ $contact ? 'contact' : 'user' }}" data-pipeline-id="{{$inquiry->pipeline_id}}" data-id="{{ $inquiry->id }}" data-move="halt" class="flat-button small blue {{$inquiry->status == 'halted' ? 'inverse' : ''}} negative-pipeline-modal-button no-comm"><i class="fa fa-thumbs-down"></i></i></button>
+    <button data-action="" data-comm-type="none" data-type="{{ $contact ? 'contact' : 'user' }}" data-pipeline-id="{{$inquiry->pipeline_id}}" data-id="{{ $inquiry->id }}" data-move="halt" class="flat-button small blue {{$inquiry->status == 'halted' ? 'inverse' : ''}} negative-pipeline-modal-button no-comm"><i class="fa fa-thumbs-down"></i></button>
 
     <!-- end halt (thumbs down) no comms -->
     <!-- pause (?) -->
@@ -28,7 +28,7 @@
     @endif 
     <!-- end forward cold and warm -->
     <!-- forward no comm -->
-    <button data-action="inquiry-pipeline" data-comm-type="default" data-type="{{ $contact ? 'contact' : 'user' }}" data-pipeline-id="{{ $inquiry->pipeline_id }}" data-id="{{ $inquiry->id }}" data-move="forward" class="flat-button small default-comm {{$inquiry->pipeline_id == 6 ? 'gray' : 'blue'}} user-managed admin" {{$inquiry->pipeline_id == 6 ? 'disabled' : ''}}><i class="fa fa-thumbs-up"></i><span class="thumbs-up-text">{{ $contact && $inquiry->pipeline_id < 2 && $inquiry->job->recruiting_type_code !== 'passive' ? ' None' : '' }}</span></button>
+    <button data-action="inquiry-pipeline" data-comm-type="none" data-type="{{ $contact ? 'contact' : 'user' }}" data-pipeline-id="{{ $inquiry->pipeline_id }}" data-id="{{ $inquiry->id }}" data-move="forward" class="flat-button small no-comm {{$inquiry->pipeline_id == 6 ? 'gray' : 'blue'}} user-managed admin" {{$inquiry->pipeline_id == 6 ? 'disabled' : ''}}><i class="fa fa-thumbs-up"></i><span class="thumbs-up-text">{{ $contact && $inquiry->pipeline_id < 2 && $inquiry->job->recruiting_type_code !== 'passive' ? ' None' : '' }}</span></button>
     <!-- end forward no comm -->
 @else
     <!-- halt (thumbs down) comms for user -->
