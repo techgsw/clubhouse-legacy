4db34a0273ca80bf9bc6e1904ebfca21f3c2569c
Sean Brown <sean@whale.enterprises>
2019-09-11 13:45:23 -0700

    Fixed organization reference and job link.

--- resources/views/emails/inquiry/notification.blade.php
+++ resources/views/emails/inquiry/notification.blade.php
@@ -1,6 +1,6 @@
 @component('emails.layout')
     @slot('body')
-        <p>{{ ucwords($inquiry_user->first_name) }} {{ ucwords($inquiry_user->last_name) }} just applied for the {{ $job->title }} position with the {{ $job->organization }}</p>
-        <p><a href="{{ config('app.url') }}/job/{{ $job->id }}">Click here</a> to view their application.</p>
+        <p>{{ ucwords($inquiry_user->first_name) }} {{ ucwords($inquiry_user->last_name) }} just applied for the <strong>{{ $job->title }}</strong> position with the <strong>{{ $job->organization->name }}</strong></p>
+        <p><a href="{{ env('CLUBHOUSE_URL') }}/job/{{ $job->id }}">Click here</a> to view their application.</p>
     @endslot
 @endcomponent
