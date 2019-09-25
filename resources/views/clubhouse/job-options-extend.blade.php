9aea087b8550e640c4d76ab456ae390487374261
James Ford <jford@albany.edu>
2019-09-25 10:57:51 -0400

    Centered modal UI elements

--- resources/views/clubhouse/job-options-extend.blade.php
+++ resources/views/clubhouse/job-options-extend.blade.php
@@ -8,10 +8,10 @@
     </div>
 </div>
 <div class="container">
-    <div class="row">
-        <p class="center-align" style="font-size: 24px; font-weight: bold;">$250.00</p>
+    <div class="center-align row">
+        <p style="font-size: 24px; font-weight: bold;">$250.00</p>
         @if ($job_type_id != JOB_TYPE_ID['sbs_default'])
-            <p class="center-align" style="font-size: 16px; font-weight: 400;">If you haven’t found the right fit for your open position, you can extend your job listing for an additional
+            <p style="font-size: 16px; font-weight: 400;">If you haven't found the right fit for your open position, you can extend your job listing for an additional
                 @if ($job_type_id == JOB_TYPE_ID['user_free'])
                     <b>30</b>
                 @elseif ($job_type_id == JOB_TYPE_ID['user_premium'])
