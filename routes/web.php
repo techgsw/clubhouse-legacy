83b1d07665e1776cf8f8a555f125abc8d6f0c43e
James Ford <james@whale.enterprises>
2020-05-13 13:37:04 -0400

    Remove unused route from old design

--- routes/web.php
+++ routes/web.php
@@ -158,8 +158,6 @@ Route::domain($domain)->group(function () {
         //endpoint for editing the user roles
         Route::get('/admin/{id}/edit-roles', 'RoleController@index');
         Route::post('/admin/{id}/update-roles', 'RoleController@update');
-
-        Route::get('/training-video/create', 'TrainingVideoController@create');
     });
 
     // Admin & Ajax
