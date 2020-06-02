f150d5625f7922f13c557422184c5345f17f2354
James Ford <james@whale.enterprises>
2020-06-02 18:12:40 -0400

    #416 Changed general formatting of blog pages. Header is now an H1. Images are large and up top. Added CTA buttons at the bottom.

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -28,6 +28,12 @@
     @section('image', $image->cdn ? $image->getURL('share') : url('/').$image->getURL('share'))
 @endif
 @section('title', $post->title)
+@if ($image_path && !is_null($image))
+    @section ('hero')
+        <div class="row hero" style="max-width:1100px;height:450px;background-image: url({{$image->getUrl('share')}});background-position: center; background-repeat: no-repeat;">
+        </div>
+    @endsection
+@endif
 @section('content')
 <div class="container">
     <div class="row">
@@ -40,32 +46,37 @@
         <div class="col s12">
             <div class="blog-post">
                 @can ('edit-post', $post)
-                    <div style="text-align: right;">
+                    <div style="float: right;">
                         <a href="/post/{{ $post->title_url }}/edit" class="flat-button blue small"><i class="fa fa-pencil"></i> Edit</a>
                     </div>
                 @endif
-                <div class="tag-list" style="margin-bottom: 12px;">
-                    @foreach ($post->tags as $tag)
-                        <a href="{{$context == 'same-here' ? '/same-here' : ''}}/blog?tag={{ urlencode($tag->slug) }}" class="flat-button gray small" style="display: inline-block; margin: 2px;">{{ ucfirst($tag->name) }}</a>
-                    @endforeach
+                <h1 class="title">{{ $post->title }}</h1>
+                <p class="author">by <?=(($post->authored_by) ?: $post->user->first_name.' '.$post->user->last_name)?> <br> {{ $post->created_at->format('F d, Y') }}</p>
+                <div style="font-size:16px;">
+                    {!! $body !!}
                 </div>
-                <h2 class="title">{{ $post->title }}</h2>
-                <div style="margin-bottom: 10px;">
-                    <a class="no-underline" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fclubhouse.sportsbusiness.solutions%2Fblog%2F<?=urlencode(htmlspecialchars($post->title_url))?>"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
-                    <a class="no-underline" target="_blank" href="https://twitter.com/intent/tweet?text=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.htmlspecialchars($post->title_url))?>"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
-                    <a class="no-underline" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.$post->title_url)?>&title=<?=htmlspecialchars($post->title)?>&summary=<?=substr($meta_body, 0, $index)?>&source=Sports Business Solutions')?>"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
-                    <a class="no-underline" href="mailto:?Subject=<?=$post->title?> | Sports Business Solutions&body=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.$post->title_url)?>"><i class="fa fa-envelope-square fa-16x" aria-hidden="true"></i></a>
+                <div class="row">
+                    <div class="col s12 center-align">
+                        <a href="{{env('APP_URL')}}" class="btn sbs-red blog-cta" style="line-height:20px;">Sports Business Solutions for C-Level and VP Level Execs</a>
+                        <a href="{{env('CLUBHOUSE_URL')}}" class="btn sbs-red blog-cta" style="line-height:20px;">Grow your professional sports career in theClubhouse</a>
+                        <a href="mailto:bob@sportsbusiness.solutions" class="btn sbs-red blog-cta">Contact Bob Hamer</a>
+                    </div>
                 </div>
-                <p class="small light uppercase">by <?=(($post->authored_by) ?: $post->user->first_name.' '.$post->user->last_name)?> | {{ $post->created_at->format('F d, Y') }}</p>
-                @if ($image_path && !is_null($image))
-                    <p class="hide-on-med-and-up" style="text-align: center;">
-                        <img style="width: 85%; max-height: auto; box-shadow: 2px 2px #F2F2F2;" src={{ $image->getURL('medium') }} />
-                    </p>
-                    <p class="hide-on-small-only" style="float: left; margin-right: 20px; margin-top: 5px;">
-                        <img style="width: auto; max-height: 300px; box-shadow: 2px 2px #F2F2F2;" src={{ $image->getURL('medium') }} />
-                    </p>
-                @endif
-                {!! $body !!}
+                <div class="row">
+                    <div class="col s12">
+                        <div class="tag-list" style="float:right;">
+                            @foreach ($post->tags as $tag)
+                                <a href="{{$context == 'same-here' ? '/same-here' : ''}}/blog?tag={{ urlencode($tag->slug) }}" class="flat-button gray small" style="display: inline-block; margin: 2px;">{{ ucfirst($tag->name) }}</a>
+                            @endforeach
+                        </div>
+                        <div style="margin-right: 10px;float:right;">
+                            <a class="no-underline" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.$post->title_url)?>&title=<?=htmlspecialchars($post->title)?>&summary=<?=substr($meta_body, 0, $index)?>&source=Sports Business Solutions')?>"><i class="fa fa-linkedin-square fa-16x" aria-hidden="true"></i></a>
+                            <a class="no-underline" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fclubhouse.sportsbusiness.solutions%2Fblog%2F<?=urlencode(htmlspecialchars($post->title_url))?>"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
+                            <a class="no-underline" target="_blank" href="https://twitter.com/intent/tweet?text=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.htmlspecialchars($post->title_url))?>"><i class="fa fa-twitter-square fa-16x" aria-hidden="true"></i></a>
+                            <a class="no-underline" href="mailto:?Subject=<?=$post->title?> | Sports Business Solutions&body=<?=urlencode('https://clubhouse.sportsbusiness.solutions/blog/'.$post->title_url)?>"><i class="fa fa-envelope-square fa-16x" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
