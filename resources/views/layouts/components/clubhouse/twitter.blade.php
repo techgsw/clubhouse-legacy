a9ca3a1b2515a1c1d329523b064562891bb0b29f
James Ford <james@whale.enterprises>
2020-06-18 08:08:40 -0700

    Modified the clubhouse footer to include an image ad in place of the newsletter

--- resources/views/layouts/components/clubhouse/twitter.blade.php
+++ resources/views/layouts/components/clubhouse/twitter.blade.php
@@ -1,7 +1,7 @@
 <div class="twitter">
     <h5>Twitter</h5>
     <div class="twitter-feed">
-        <a class="twitter-timeline" data-chrome="nofooter transparent noheader transparent" data-height="425" href="https://twitter.com/theC1ubhouse"></a>
+        <a class="twitter-timeline" data-chrome="nofooter transparent noheader transparent" data-height="225" href="https://twitter.com/theC1ubhouse"></a>
         <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
 </div>
