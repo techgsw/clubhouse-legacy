9879e3657d86d5a96267ec829a74a76e734774fa
James Ford <james@whale.enterprises>
2020-12-31 17:13:59 -0500

    Fix for image width with Outlook

--- resources/views/emails/signature-footer.blade.php
+++ resources/views/emails/signature-footer.blade.php
@@ -1,3 +1,3 @@
-<p><img width="110px" src="{{ asset('images/bob_hamer_signature.png') }}" /></p>
+<p><img width="110" style="width:110px;" src="{{ asset('images/bob_hamer_signature.png') }}" /></p>
 <p>Bob Hamer<br/>Founder, <span style="color: #EB2935;">the</span>Clubhouse</p>
 
