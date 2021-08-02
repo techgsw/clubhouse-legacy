313bf9fecbaf181c251f237a7659fd78ec445096
James Ford <james@whale.enterprises>
2021-08-02 13:50:56 -0400

    Added "manage preferences" blade

---
+++ resources/views/emails/footer-manage-preferences.blade.php
@@ -0,0 +1,5 @@
+<tr>
+    <td align="center" style="padding-bottom:20px;">
+        <a href="{{env('CLUBHOUSE_URL')}}/user/{{$user->id}}/edit-profile" style="font-size:.9em">Manage Email Preferences</a>
+    </td>
+</tr>
