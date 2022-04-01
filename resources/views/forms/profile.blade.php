821ab441723577fb323becdc2d4016c4e617dc0b
Jonathon Klem <jonathonklem@gmail.com>
2022-04-01 06:43:45 -0700

    Remove green checkbox for when the linkedin url is set

--- resources/views/forms/profile.blade.php
+++ resources/views/forms/profile.blade.php
@@ -48,9 +48,7 @@
                     <input id="linkedin" name="linkedin" type="text" value="{{ $profile->linkedin }}">
                     <label for="linkedin" class="active" style="width: 95%">
 			LinkedIn Profile URL
-                	@if ($profile->linkedin)
-                	    <span class="progress-icon progress-complete green-text text-darken-2" style="float: right;"><i class="material-icons">check_circle</i></span>
-                	@else
+                	@if (!$profile->linkedin)
                 	    <span class="progress-icon progress-incomplete yellow-text text-darken-2" style="float: right;"><i class="material-icons">warning</i></span>
                 	@endif
 		    </label>
