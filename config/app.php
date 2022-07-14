ce1c0edd307b6d063f8c62e534c684c95e80d07f
Mark Pemburn <markpemburn@trak.io>
2022-07-14 16:23:27 -0400

    Change SBS Consulting to General Sports Worldwide for email footers, etc.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'SBS Consulting',
+    'name' => 'General Sports Worldwide',
 
     /*
     |--------------------------------------------------------------------------
