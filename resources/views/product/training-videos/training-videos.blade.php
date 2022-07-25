61c58055a917bf72585de204888c12a1cd7175b3
Mark Pemburn <markpemburn@trak.io>
2022-07-25 13:30:39 -0400

    Clean up layout.

--- resources/views/product/training-videos/training-videos.blade.php
+++ resources/views/product/training-videos/training-videos.blade.php
@@ -15,12 +15,12 @@
     }
 @endphp
 @section('content')
-<div class="container">
-    @if(count($videos) < 1)
-        <div class="row center-align">
-            <h5>Training Videos coming soon.</h5>
-        </div>
-    @else
+    <div class="container">
+        @if(count($videos) < 1)
+            <div class="row center-align">
+                <h5>Training Videos coming soon.</h5>
+            </div>
+        @else
             <div class="row">
                 @include('components.page-search', ['base_url' => '/training/training-videos'])
             </div>
@@ -28,22 +28,22 @@
                 @include('components.page-search-found', ['base_url' => '/training/training-videos', 'items' => $videos, 'not_found' => $active_tag])
             @endif
 
-        <div class="row">
-            @foreach($videos->chunk(6) as $chunked_videos)
-                <div class="row" style="max-height: 0;">
-                    @foreach($chunked_videos as $video)
-                        <div class="col s12 m6 l4" >
-                            @include('product.training-videos.components.list-item', ['video' => $video])
-                        </div>
-                    @endforeach
+            <div class="row">
+                @foreach($videos->chunk(6) as $chunked_videos)
+                    <div class="row" style="max-height: 0;">
+                        @foreach($chunked_videos as $video)
+                            <div class="col s12 m6 l4">
+                                @include('product.training-videos.components.list-item', ['video' => $video])
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+                <div class="row">
+                    <div class="col s12 center-align">
+                        {{ $videos->appends(request()->all())->fragment('past')->links('components.pagination') }}
+                    </div>
                 </div>
-            @endforeach
-        <div class="row">
-            <div class="col s12 center-align">
-                {{ $videos->appends(request()->all())->fragment('past')->links('components.pagination') }}
             </div>
-        </div>
-    </div>
-    @endif
-    <br><br>
+        @endif
+        <br><br>
 @endsection
--- resources/views/product/webinars.blade.php
+++ resources/views/product/webinars.blade.php
@@ -10,71 +10,72 @@
 @endphp
 
 @section('content')
-<div class="container">
-    <div class="col s12">
-        @include('layouts.components.messages')
-        @include('layouts.components.errors')
-    </div>
-    @if(is_null($active_tag))
-        <div class="row" style="margin-bottom: unset;">
-            <div class="col s12">
-                <h4 id="upcoming" style="font-weight: bold; text-align: center;">UPCOMING WEBINAR EVENTS</h4>
-            </div>
+    <div class="container">
+        <div class="col s12">
+            @include('layouts.components.messages')
+            @include('layouts.components.errors')
         </div>
-        <div class="row">
-            @if (count($active_products) < 1)
-                <h5 style="text-align: center;">Coming soon.</h5>
-            @else
-                @foreach ($active_products as $product)
+        @if(is_null($active_tag))
+            <div class="row" style="margin-bottom: unset;">
+                <div class="col s12">
+                    <h4 id="upcoming" style="font-weight: bold; text-align: center;">UPCOMING WEBINAR EVENTS</h4>
+                </div>
+            </div>
+            <div class="row">
+                @if (count($active_products) < 1)
+                    <h5 style="text-align: center;">Coming soon.</h5>
+                @else
+                    @foreach ($active_products as $product)
                         <div class="col l6" style="display: flex; justify-content: center;">
                             @include('product.webinars.components.list-item', ['product' => $product])
                         </div>
-                @endforeach
-            @endif
-        </div>
-        <hr class="sbs-red" style="color: #EB2935;" />
-    @endif
-    @if (count($inactive_products) > 0)
-        <div class="row">
-            <div class="col s12">
-                <h4 id="past" style="font-weight: bold; text-align: center;">PAST WEBINAR EVENTS</h4>
-            </div>
-            <div class="col s12 center-align" style="margin-top:20px;">
-                <strong><span class="sbs-red-text">FREE Sign up</span>: Watch with a free membership</strong>
-                <br>
-                <strong><span class="sbs-red-text">PRO Member</span>: Watch with a Clubhouse PRO membership</strong>
-                <br>
-                <strong><span class="sbs-red-text">WATCH NOW</span>: No signup required</strong>
-            </div>
-        </div>
-        <div class="row">
-            @include('components.page-search', ['base_url' => '/webinars'])
-        </div>
-        @if (request('search') || request('tag'))
-            @include('components.page-search-found', ['base_url' => '/webinars', 'items' => $inactive_products, 'not_found' => $active_tag])
-        @endif
-        <div class="row">
-            @foreach ($inactive_products->chunk(6) as $chunked_products)
-                <div class="row" style="max-height: 0;">
-                    @foreach($chunked_products as $product)
-                        <div class="col s12 m6 l4" >
-                            @include('product.webinars.components.inactive-list-item', ['$product' => $product])
-                        </div>
                     @endforeach
+                @endif
+            </div>
+            <hr class="sbs-red" style="color: #EB2935;"/>
+        @endif
+        @if (count($inactive_products) > 0)
+            <div class="row">
+                <div class="col s12">
+                    <h4 id="past" style="font-weight: bold; text-align: center;">PAST WEBINAR EVENTS</h4>
                 </div>
-            @endforeach
-        </div>
-        <div class="row">
-            <div class="col s12 center-align">
-                {{ $inactive_products->appends(request()->all())->fragment('past')->links('components.pagination') }}
+                <div class="col s12 center-align" style="margin-top:20px;">
+                    <strong><span class="sbs-red-text">FREE Sign up</span>: Watch with a free membership</strong>
+                    <br>
+                    <strong><span class="sbs-red-text">PRO Member</span>: Watch with a Clubhouse PRO membership</strong>
+                    <br>
+                    <strong><span class="sbs-red-text">WATCH NOW</span>: No signup required</strong>
+                </div>
             </div>
-        </div>
-    @elseif (!is_null($active_tag))
-        <div class="row">
-            <div class="col s12">
-                <h4 id="past" style="font-weight: bold; text-align: center;">No past webinar events matching <strong>{{ $active_tag->name }}</strong></h4>
+            <div class="row">
+                @include('components.page-search', ['base_url' => '/webinars'])
             </div>
-        </div>
-    @endif
-</div>
+            @if (request('search') || request('tag'))
+                @include('components.page-search-found', ['base_url' => '/webinars', 'items' => $inactive_products, 'not_found' => $active_tag])
+            @endif
+            <div class="row">
+                @foreach ($inactive_products->chunk(6) as $chunked_products)
+                    <div class="row" style="max-height: 0;">
+                        @foreach($chunked_products as $product)
+                            <div class="col s12 m6 l4">
+                                @include('product.webinars.components.inactive-list-item', ['$product' => $product])
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
+            <div class="row">
+                <div class="col s12 center-align">
+                    {{ $inactive_products->appends(request()->all())->fragment('past')->links('components.pagination') }}
+                </div>
+            </div>
+        @elseif (!is_null($active_tag))
+            <div class="row">
+                <div class="col s12">
+                    <h4 id="past" style="font-weight: bold; text-align: center;">No past webinar events matching
+                        <strong>{{ $active_tag->name }}</strong></h4>
+                </div>
+            </div>
+        @endif
+    </div>
 @endsection
