5ea1f8d9039cf356de7619b194a61555e7854ec1
Mark Pemburn <markpemburn@trak.io>
2022-07-25 10:30:10 -0400

    Change top nav desktop layout.

--- resources/views/layouts/components/clubhouse/header.blade.php
+++ resources/views/layouts/components/clubhouse/header.blade.php
@@ -3,17 +3,23 @@
     <nav class="nav-main" role="navigation">
         <div class="nav-wrapper container">
             <!--<a href="/" style="color: #3F3E3E; left: 0; margin-top: 25px;" class="brand-logo">CLUBHOUSE</a>-->
-            <a href="/" class="brand-logo left-align custom-brand-logo">
-                <img src="/images/logos/full/CH_logo_color.jpg?v=1" alt="SBS Consulting">
-            </a>
-            <ul class="right hide-on-med-and-down nav-custom-desktop" style="margin-right: -50px;">
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/webinars">Webinars</a></li>
-                <li><a href="/mentor">Mentors</a></li>
-                <li><a href="/job">Job Board</a></li>
-                <li><a href="/training">Training</a></li>
-                <li><a href="/same-here">Mental Health</a></li>
-            </ul>
+            <div class="row" >
+                <a href="/" class="brand-logo left-align custom-brand-logo">
+                    <img src="/images/logos/full/CH_logo_color.jpg?v=1" alt="SBS Consulting" style="margin-bottom: 2.0rem;">
+                </a>
+            </div>
+            <div class="row" style=" margin-top: 3rem;">
+                <ul class="hide-on-med-and-down nav-custom-desktop" style="position: relative; z-index: 999;">
+                    <li><a href="/our-team">Our Team</a></li>
+                    <li><a href="/job">Job Board</a></li>
+                    <li><a href="/mentor">Mentorship</a></li>
+                    <li><a href="/pro-membership">PRO Membership</a></li>
+                    <li><a href="/training">Training Vault</a></li>
+                    <li><a href="/webinars">Webinars</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/same-here">Mental Health</a></li>
+                </ul>
+            </div>
             <ul id="nav-mobile-clubhouse" class="side-nav">
                 <li class="social-media">
                     <a href="https://www.facebook.com/TheClubhouse-458907761265631" style="max-width: 20px"><i class="fa fa-facebook-square fa-16x" aria-hidden="true"></i></a>
@@ -23,11 +29,13 @@
                 </li>
                 <li><a href="https://generalsportsworldwide.com" style="text-transform: none; font-size: 18px;">General Sports Worldwide</a></li>
                 <li class="divider"></li>
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/webinars">Webinars</a></li>
-                <li><a href="/mentor">Mentors</a></li>
+                <li><a href="/our-team">Our Team</a></li>
                 <li><a href="/job">Job Board</a></li>
-                <li><a href="/training">Training</a></li>
+                <li><a href="/mentor">Mentorship</a></li>
+                <li><a href="/pro-membership">PRO Membership</a></li>
+                <li><a href="/training">Training Vault</a></li>
+                <li><a href="/webinars">Webinars</a></li>
+                <li><a href="/blog">Blog</a></li>
                 <li><a href="/same-here">Mental Health</a></li>
                 <li class="divider"></li>
                 @if (Auth::guest())
