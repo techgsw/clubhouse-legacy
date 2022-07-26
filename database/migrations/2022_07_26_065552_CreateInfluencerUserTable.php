75673d11ec7633225239ed94a125ba4474c9a33f
Mark Pemburn <markpemburn@trak.io>
2022-07-26 10:23:28 -0400

    Create migration for influencers and influencer_user tables.

---
+++ database/migrations/2022_07_26_065552_CreateInfluencerUserTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInfluencerUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('influencer_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('influencer_id')->unsigned();
+            $table->foreign('influencer_id')->references('id')->on('influencers');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('influencer_user');
+    }
+}
