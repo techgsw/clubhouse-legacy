e895761cb11ac857a158c2047f0088b86229d080
Mark Pemburn <markpemburn@trak.io>
2022-07-27 15:14:00 -0400

    Deprecate reference to Same Here Instagram feed.

--- app/Http/Controllers/SocialMediaController.php
+++ app/Http/Controllers/SocialMediaController.php
@@ -11,9 +11,11 @@ class SocialMediaController extends Controller
 
     public function instagram(Request $request)
     {
+        /*  This appears to be deprecated
         if ($request->get('is_same_here') == 'true') {
             return SocialMediaServiceProvider::getInstagramFeed(view('layouts.components.same-here-instagram-feed'), 'same-here');
         }
+        */
 
         return view('layouts.components.instagram-feed');
     }
