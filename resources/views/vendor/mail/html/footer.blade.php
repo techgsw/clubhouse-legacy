b420b5e8ec3b15ca4f2e25097c7bee273f3bf963
Mark Pemburn <mpemburn@gmail.com>
2022-08-04 06:51:24 -0700

    Add footer change.

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,7 +3,7 @@
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
             <tr>
                 <td class="content-cell" align="center">
-                    <p class="small">Copyright &copy; {{ date('Y') }} {{ __('general.company_name') }} | All Rights Reserved</p>
+                    <p class="small">Copyright © 2017 SBS Consulting | All Rights Reserved</p>
                 </td>
             </tr>
         </table>
