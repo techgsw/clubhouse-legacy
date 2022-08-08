b26eab7c80280c888e1d6a8269b25268bd043a87
Mark Pemburn <markpemburn@trak.io>
2022-08-08 06:05:47 -0400

    Remove deprecated Instagram feed code.

--- app/Http/Controllers/SocialMediaController.php
+++ app/Http/Controllers/SocialMediaController.php
@@ -9,17 +9,6 @@ use Illuminate\Support\Facades\Auth;
 class SocialMediaController extends Controller
 {
 
-    public function instagram(Request $request)
-    {
-        /*  This appears to be deprecated
-        if ($request->get('is_same_here') == 'true') {
-            return SocialMediaServiceProvider::getInstagramFeed(view('layouts.components.same-here-instagram-feed'), 'same-here');
-        }
-        */
-
-        return view('layouts.components.instagram-feed');
-    }
-
     public function twitter(Request $request)
     {
         return SocialMediaServiceProvider::getTweets(view('layouts.components.twitter-hashtag-feed'), $request->get('context'));
